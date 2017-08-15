6b8360e2d689ae60c59fa201a505102f5837476c
Matheus Fidelis <msfidelis@outlook.com>
2017-08-15 20:25:38 -0300

    listagem de bancos

--- test.js
+++ test.js
@@ -8,21 +8,4 @@ const chave = "ef947cf5867488f744b82744dd3a8fc4852e529f";
 
 const PJBank = new PJBankSDK(credencial, chave);
 
-const DadosCartao = {
-    "nome_cartao": "Cliente Exemplo",
-    "numero_cartao": "4012001037141112",
-    "mes_vencimento": "05",
-    "ano_vencimento": "2018",
-    "cpf_cartao": "64111456529",
-    "email_cartao": "api@pjbank.com.br",
-    "celular_cartao": "978456723",
-    "codigo_cvv": "123"
-};
-
-PJBank.Recebimento.Cartao.Tokenizar(DadosCartao)
-    .then((tokenizacao) => {
-        console.log(tokenizacao);
-    })
-    .catch((err) => {
-        console.log(err);
-    })
+console.log(PJBank.Recebimento.Bancos);
