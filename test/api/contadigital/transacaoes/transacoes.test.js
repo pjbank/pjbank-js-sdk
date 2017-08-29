1fe1dc3b6eb4f43c41f1f1b6d7d700699e171f4a
Matheus Fidelis <msfidelis@outlook.com>
2017-08-29 09:57:06 -0300

    Transações em lotes

--- test/api/contadigital/transacaoes/transacoes.test.js
+++ test/api/contadigital/transacaoes/transacoes.test.js
@@ -89,4 +89,61 @@ suite("#CONTADIGITAL - #Transacoes", () => {
 
     });
 
+
+    test('Gerando despesas em lotes', (done) => {
+
+        const PJBank = new PJBankSDK(credencialContaDigital, chaveContaDigital);
+
+        const Transacoes = [{
+                "data_vencimento": "08/30/2017",
+                "data_pagamento": "08/30/2017",
+                "valor": 20.00,
+                "banco_favorecido": "033",
+                "agencia_favorecido": "1111-X",
+                "conta_favorecido": "11111",
+                "cnpj_favorecido": "45475754000136",
+                "nome_favorecido": "Cliente Exemplo",
+                "identificador": "123123",
+                "descricao": "Descrição de exemplo",
+                "solicitante": "Teste DOC/TED",
+                "tipo_conta_favorecido": "corrente"
+            },
+            {
+                "data_vencimento": "09/30/2017",
+                "data_pagamento": "09/30/2017",
+                "valor": 10.50,
+                "codigo_barras": "03399699255873781001843279301014571980000001000"
+            }
+        ];
+
+        PJBank.ContaDigital.Transacao(Transacoes)
+            .then((transacoes) => {
+
+                expect(transacoes).to.have.property('status');
+                assert.equal(transacoes.status, 201);
+
+                expect(transacoes).to.have.property('data');
+                expect(transacoes.data).to.be.a('array');
+
+                expect(transacoes.data[0]).to.have.property('msg');
+                expect(transacoes.data[0]).to.have.property('id_operacao');
+                expect(transacoes.data[0]).to.have.property('data_pagamento');
+                expect(transacoes.data[1]).to.have.property('msg');
+                expect(transacoes.data[1]).to.have.property('id_operacao');
+                expect(transacoes.data[1]).to.have.property('data_pagamento');
+
+                done();
+
+            })
+            .catch((err) => {
+                console.log(err);
+                done();
+            });
+
+
+
+    });
+
+
+
 });
